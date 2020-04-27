8e6e7c8c6f2bf5cd7211b376902fc0ea3bec0e1c
Anuj <anujkumar@g2techsoft.com>
2020-04-27 12:18:08 +0530

    iOS_Code_Exercise project set_up

---
+++ iOS_Code_Exercise_Telstra/iOS_Code_Exercise_Telstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Code_Exercise_Telstra
+//
+//  Created by ANUJ KUMAR on 27/04/20.
+//  Copyright © 2020 ANUJ KUMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
