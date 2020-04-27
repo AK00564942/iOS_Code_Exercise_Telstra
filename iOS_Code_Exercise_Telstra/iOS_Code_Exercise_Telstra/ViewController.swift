1969bcad7893d478ae5da77c4a7153c986e8e97b
Anuj <anujkumar@g2techsoft.com>
2020-04-27 18:23:08 +0530

    Dataload SetUp

--- iOS_Code_Exercise_Telstra/iOS_Code_Exercise_Telstra/ViewController.swift
+++ iOS_Code_Exercise_Telstra/iOS_Code_Exercise_Telstra/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+     let tableView = UITableView()
+     var safeArea: UILayoutGuide!
+    
+   var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        safeArea = view.layoutMarginsGuide
+        tableView.dataSource = self
+        
+        setupView()
     }
-
-
+    
+    //MARK:- SetUp View
+    func setupView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+      }
+    }
+//MARK:- UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 7
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "Hi Anuj "
+    return cell
 }
+}
+
 
